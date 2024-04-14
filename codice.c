bd4bbfcec0ff7c50fa91ed27bb0ac8541b424184
Matteo Darpin <matteo.darpin@studente.unipd.it>
2024-04-14 11:32:09 +0200

    Finalmente sono riuscito a sistemare l'errore Segmentation Fault, cos'ho imparato? Ogni tanto è bene chiudere visual studio e riaprirlo perchè anche se il codice è stato corretto il terminale CONTINUA A DARE LO STESSO ERRORE, mentre, MAGICAMENTE, con un riavvio, l'errore sparisce e posso passasre alla soluzione del 4° problema.

--- codice.c
+++ codice.c
@@ -18,20 +18,9 @@ void estrai_dati(int ac, char** av, int* vett, int* lung) {
 		vett[i] = atoi(av[i + 1]);
 }
 
-//es: vett[2] = 4
-// posizione = 1
-//
-//posizione = posizione array dati ordinati a cui sono arrivato
-//e nella quale ho nuovo dato < del dato già presente
-//
-//vett = vettore dati ordinati
-//
-//lung = il numero corrispondente alla posizione nell'array
-//dei nuovi dati da inserire
-
 void fai_spazio(int posizione, int* vett, int lung) {
-	for (int j = lung - 1; j > posizione; ++j)
-		vett[j] = vett[j - 1];
+	for (int j = lung - 1; j >= posizione; --j)
+		vett[j + 1] = vett[j];
 }
 
 void inserisci(int nuovo_dato, int num_dati_ord, int* vett) {
