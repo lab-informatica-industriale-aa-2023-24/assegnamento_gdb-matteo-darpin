c65b4516576d15e9a637a22d9cce56c8b85e88a1
Matteo Darpin <matteo.darpin@studente.unipd.it>
2024-04-14 11:03:03 +0200

    preannuncio che ho perso tutti i vecchi commit in quanto mi sono reso conto tardi che la cartella con il mio nome utente non era stata creata su gitub ho dovuto accettare nuovamente la consegna e clonare dinuovo il tutto nella cartella assegnamenti; elenco di seguito le principali modifiche: formattazione del file codice.c, corretto errore 1 (riga di codice per ottenere la dimensione dell'array), corretto errore 2 (== nell'IF, sto lavorando sull'errore 2segmentation fault presumibilmente posto nella funzione fai spazio (si sta scrivendo su posizioni dell'array non allocate)

--- codice.c
+++ codice.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * codice per insertion sort
  * descrizione: https://it.wikipedia.org/wiki/Insertion_sort
  */
@@ -8,29 +8,40 @@
 
 #define MAX_INPUT 10
 
-void estrai_dati(int ac, char **av, int *vett, int *lung)
-{
-	*lung = ac - 1;
+void estrai_dati(int ac, char** av, int* vett, int* lung) {
+	*lung = ac - 1; //ac contiene anche il nome del file
 
+	//converte i numeri inseriti da linea di comando
+	//(stringhe) in interi e li salva nel vettore
+	//appositamente creato
 	for (int i = 0; i < *lung; ++i)
-		vett[i] = atoi(av[i+1]);
+		vett[i] = atoi(av[i + 1]);
 }
 
-void fai_spazio(int posizione, int *vett, int lung)
-{
-	for (int j = lung - 1; j > posizione; ++j) 
-		vett[j] = vett[j-1];
+//es: vett[2] = 4
+// posizione = 1
+//
+//posizione = posizione array dati ordinati a cui sono arrivato
+//e nella quale ho nuovo dato < del dato già presente
+//
+//vett = vettore dati ordinati
+//
+//lung = il numero corrispondente alla posizione nell'array
+//dei nuovi dati da inserire
+
+void fai_spazio(int posizione, int* vett, int lung) {
+	for (int j = lung - 1; j > posizione; ++j)
+		vett[j] = vett[j - 1];
 }
 
-void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
-{ 
-	if (num_dati_ord = 0)  { // il vettore è vuoto, facile
+void inserisci(int nuovo_dato, int num_dati_ord, int* vett) {
+	if (num_dati_ord == 0) { // il vettore è vuoto, facile
 		vett[0] = nuovo_dato;
 		return;
 	}
 
-	for (int i = 0; i < num_dati_ord; ++i)  {
-		if (nuovo_dato < vett[i])  {
+	for (int i = 0; i < num_dati_ord; ++i) {
+		if (nuovo_dato < vett[i]) {
 			// sposta da vett[i] in poi di un posto sulla destra
 			// prima di inserire il nuovo_dato
 			fai_spazio(i, vett, num_dati_ord);
@@ -38,34 +49,36 @@ void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
 			return;
 		}
 	}
+	vett[num_dati_ord] = nuovo_dato;
 }
 
-void ordina_dati(const int *dati_non_ordinati, int *dati_ordinati)
-{
-	int num_dati = sizeof(dati_non_ordinati) / sizeof(dati_non_ordinati[0]);
+void ordina_dati(const int* dati_non_ordinati, int* dati_ordinati, int num_dati) {
+
 	for (int i = 0; i < num_dati; ++i)
 		inserisci(dati_non_ordinati[i], i, dati_ordinati);
 }
 
-void stampa_vettore(const int *vett, int lung)
-{
+void stampa_vettore(const int* vett, int lung) {
 	for (int i = 0; i < lung; ++i)
-		printf("%d ",vett[i]);
+		printf("%d ", vett[i]);
 	printf("\n");
 }
 
-int main(int argc, char **argv)
-{
+// argc = numero argomenti, argv = vettore puntatori
+int main(int argc, char** argv) {
 	if (argc > MAX_INPUT + 1) {
 		printf("Numero massimo di input %d\n", MAX_INPUT);
 		return -1;
 	}
-	int dati_input[MAX_INPUT] = {0};
-	int dati_ordinati[MAX_INPUT] = {0};
+	int dati_input[MAX_INPUT] = { 0 };
+	int dati_ordinati[MAX_INPUT] = { 0 };
 	int num_dati = 0;
 
 	estrai_dati(argc, argv, dati_input, &num_dati);
-	ordina_dati(dati_input, dati_ordinati);
+
+	int n_dati = sizeof(dati_input) / sizeof(dati_ordinati[0]);
+
+	ordina_dati(dati_input, dati_ordinati, n_dati);
 	stampa_vettore(dati_ordinati, num_dati);
 	return 0;
 }
